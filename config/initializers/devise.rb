70d2893b778dae16fe01efeafaced03c4612cb9e
Medhi Widjaja <medhiwidjaja@yahoo.com>
2018-01-18 10:32:32 +0700

    Init settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,6 +240,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 
-  config.omniauth :google_oauth2, Rails.application.secrets[:google_client_id],
-      Rails.application.secrets[:google_client_secret]
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  
 end
